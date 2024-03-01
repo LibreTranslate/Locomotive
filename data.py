0633c79773c98cddfc44b9a9a8a327642ae3aad1
lenicolas <34744689+lecoqnicolas@users.noreply.github.com>
2024-03-01 18:14:03 +0100

    Update data.py
    Might work after all

--- data.py
+++ data.py
@@ -316,7 +316,8 @@ def merge_shuffle(sources, out_dir, max_eval_sentences=5000, remove_duplicates=T
                 # Skip top % if excerpt filter on
                 if begin_at is not None and count <= begin_at
                     continue
-                
+
+                #Exits if excerpt or top filter on
                 if stop_at is not None and count >= stop_at:
                     break
 
