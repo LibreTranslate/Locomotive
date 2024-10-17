f52e7575ccb1c275b801b424089203bf5c0c6aef
lenicolas <34744689+lecoqnicolas@users.noreply.github.com>
2024-10-17 12:12:44 +0200

    Update ckfilter.py

--- ckfilter.py
+++ ckfilter.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2020 Unbabel
+# Copyright (C) 2022 Unbabel
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the CC-BY-NC-SA4.0 licence;
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.en
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
